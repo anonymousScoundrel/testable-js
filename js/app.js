bafba4b7709d593de74c0b88ffb6cf71c1672146
Theo Coukoulis <root@addmorehacks.com>
2015-08-21 03:32:24 -0500

    Learning about coupling

--- js/app.js
+++ js/app.js
@@ -1,6 +1,28 @@
 (function ($, undefined) {
   "use strict";
   $(document).ready(function () {
-    $(document).foundation();
+
+    $(document).foundation().foundation(
+      'joyride',
+      'start',
+      { post_ride_callback: function () {
+        $('#first-ride').addClass('hide');
+      }}
+    );
+
+    $('.close-parent').off('click').on('click', function () {
+      $(this).closest('.row').addClass('hide');
+    });
+    $('.code-example-switch').off('click').on('click', function () {
+        var self = $(this);
+        var codeExample = self.closest('.example-switch').next('.example-code');
+
+        if (self.is(':checked')) {
+          codeExample.removeClass('hide');
+        } else {
+          codeExample.addClass('hide');
+        }
+    });
+
   });
 }(jQuery));
